ff409e1d8b7ff2ee6623a859740697e3b837612b
user <user@users-mbp>
2018-08-21 18:14:24 +0200

    Initial Commit

---
+++ TruDocPhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TruDocPhotos
+//
+//  Created by Nermeen Tomoum on 8/21/18.
+//  Copyright © 2018 NrmeenTomoum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TruDocPhotos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TruDocPhotos
+//
+//  Created by Nermeen Tomoum on 8/21/18.
+//  Copyright © 2018 NrmeenTomoum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
